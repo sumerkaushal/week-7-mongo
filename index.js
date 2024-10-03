265858920346ded612021bd99fd1d45d46af2d6d
sumerkaushal <sumerkaushal95@gmail.com>
2024-10-03 18:07:35 -0400

    Improvements has  been made by using bcrypt and zod for secure and better functionality

--- index.js
+++ index.js
@@ -1,83 +1,118 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const { UserModel, TodoModel } = require("./db");
 const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const { z } = require('zod')
 
 mongoose.connect("")
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function(req, res) {
+app.post("/signup", async (req, res) => {
+    const requiredBody = z.object({
+        email : z.string().min(5).max(50).email(),
+        name : z.string().min(1).max(50),
+        password : z.string().min(8).max(20)
+    })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess){
+        res.json({
+            message: "Incorrect Credentials"
+        })
+    }
+
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
-
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
     
-    res.json({
-        message: "You are signed up"
-    })
-});
-
-
-app.post("/signin", async function(req, res) {
+   
+      const hashedPassword = await bcrypt.hash(password, 5);
+  
+      await UserModel.create({
+        email,
+        password: hashedPassword,
+        name,
+      });
+    
+      res.json({
+        message: "User already exist",
+      });
+      
+   
+  
+    if(!errorThrown){
+      res.json({
+        message: "You are signed in",
+      });
+    }
+  
+  
+  });
+  
+  app.post("/signin", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-
+  
     const response = await UserModel.findOne({
-        email: email,
-        password: password,
+      email: email,
     });
-
-    if (response) {
-        const token = jwt.sign({
-            id: response._id.toString()
-        }, JWT_SECRET);
-
-        res.json({
-            token
-        })
-    } else {
-        res.status(403).json({
-            message: "Incorrect creds"
-        })
+  
+    if (!user) {
+      res.json({
+        message: "User does not exist",
+      });
     }
-});
-
-
-app.post("/todo", auth, async function(req, res) {
-    const userId = req.userId;
-    const title = req.body.title;
+  
+    const matchPassword = await bcrypt.compare(password, response.password);
+  
+    if (matchPassword) {
+      const token = jwt.sign(
+        {
+          id: response._id.toString(),
+        },
+        JWT_SECRET
+      );
+      res.json({
+        token,
+      });
+    } else {
+      res.status(403).json({
+        message: "Wrong Credentials",
+      });
+    }
+  });
+  
+  app.post("/todo", auth, async (req, res) => {
     const done = req.body.done;
-
-    await TodoModel.create({
-        userId,
-        title,
-        done
-    });
-
-    res.json({
-        message: "Todo created"
-    })
-});
-
-
-app.get("/todos", auth, async function(req, res) {
+    const title = req.body.title;
     const userId = req.userId;
-
-    const todos = await TodoModel.find({
-        userId
+  
+    await TodoModel.create({
+      userId,
+      title,
+      done,
     });
-
     res.json({
-        todos
-    })
-});
+      message: "Todo Created",
+    });
+  });
+  
+  app.get("/todos", auth, async (req, res) => {
+    const userId = req.userId;
+    await TodoModel.find({
+      userId,
+    });
+    res.json({
+      userId,
+    });
+  });
+  
+  
+
 
 app.listen(3000);
